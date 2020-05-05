f8487d0cec3ff96436d2a03a18cc30426ae6d17c
Acer_Cristea <ac01064@srcs.org>
2020-05-04 22:40:27 -0700

    I went beast mode. Added drop in menu and changing background.

--- algorithms.js
+++ algorithms.js
@@ -36,4 +36,20 @@ $(document).keydown(function(e){
     }
 });
 
+$(function () {
+    var body = $('body');
+    var backgrounds = [
+      'url(https://www.mobygames.com/images/shots/l/755414-geometry-dash-android-screenshot-flying-in-a-short-sequence.jpg)',
+      'url(https://gamehelp.guru/wp-content/uploads/2016/01/Geometry-Dash-Screenshot-1.jpg)'];
+    var current = 0;
 
+    function nextBackground() {
+        body.css(
+            'background',
+        backgrounds[current = ++current % backgrounds.length]);
+
+        setTimeout(nextBackground, 5000);
+    }
+    setTimeout(nextBackground, 5000);
+    body.css('background', backgrounds[0]);
+});
