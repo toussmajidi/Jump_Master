f85fba5c1d8e9a0257748efcd1d54d12d0690545
toussmajidi <toussmajidi@yahoo.com>
2020-05-07 14:57:55 -0700

    Works now!

--- GameAlgorithms.js
+++ GameAlgorithms.js
@@ -1,27 +1,34 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-canvas.width = 480;
-canvas.height = 320;
-var x = canvas.width/2;
-var y = canvas.height-30;
+canvas.width = 600;
+canvas.height = 180;
+var x = canvas.width * (2 / 3);
+var y = canvas.height - 50;
 var dx = 2;
 var dy = -2;
 
 
 var img = new Image();
-img.src = "https://www.theappguruz.com/app/uploads/2015/06/create-grass.png";
-window.onload = function(){
+img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRL4TkLZoF7w69JWH13dlIlSq19DbFqxb1fuIL0m9lYTVVpPwaW&usqp=CAU";
+window.onload = function () {
     var scrollspeed = 10;
     var imgWidth = 0;
-    function loop()
-    {
+    var time = new Date();
+
+    function loop() {
 
         ctx.drawImage(img, imgWidth, 0);
 
         ctx.drawImage(img, imgWidth - canvas.width, 0);
 
+        ctx.beginPath();
+        ctx.arc(x, y, 10, 0, Math.PI * 2);
+        ctx.fillStyle = "red";
+        ctx.fill();
+        ctx.closePath();
 
-        imgWidth += scrollspeedpeed;
+
+        imgWidth += scrollspeed;
 
         if (imgWidth == canvas.width)
             imgWidth = 0;
@@ -30,22 +37,4 @@ window.onload = function(){
     }
 
     loop();
-
 }
-
-/*function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
-}
-
-/*function draw() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
-    x += dx;
-    y += dy;
-}
-
-setInterval(draw, 10);
