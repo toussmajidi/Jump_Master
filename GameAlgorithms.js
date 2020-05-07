a1b3f188263829306f62172a92e19ee97fba280f
Acer_Cristea <ac01064@srcs.org>
2020-05-06 22:42:55 -0700

    Done

--- GameAlgorithms.js
+++ GameAlgorithms.js
@@ -2,6 +2,8 @@ var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 var x = canvas.width/2;
 var y = canvas.height-30;
+var dx = 2;
+var dy = 0;
 
 
 
@@ -16,6 +18,8 @@ function drawBall() {
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawBall();
+    x += dx;
+    y += dy;
 }
 
 setInterval(draw, 10);
