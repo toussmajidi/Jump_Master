b214b25a66e3c729b9e72828b8ffa250d9874820
toussmajidi <toussmajidi@yahoo.com>
2020-05-09 23:36:40 -0700

    jumping?

--- GameAlgorithms.js
+++ GameAlgorithms.js
@@ -4,16 +4,36 @@ canvas.width = 1000;
 canvas.height = 400;
 var x = canvas.width * (2 / 3);
 var y = canvas.height - 100;
-var dx = 2;
-var dy = -2;
+var dy = 50;
+var gravity = -4.9;
+var time = new Date();
 
+window.addEventListener("keypress", jump);
+
+function jump() {
+    var t = time/1000;
+    if (event.key === "b"){
+    y = y + ((gravity * Math.pow(t,2)) + dy);
+    }
+    if(y <= 0){
+        y++;
+    }
+    }
+
+
+function draw() {
+    ctx.beginPath();
+    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.fillStyle = "red";
+    ctx.fill();
+    ctx.closePath();
+}
 
 var img = new Image();
 img.src = "Savannah_Level.png";
 window.onload = function () {
     var scrollspeed = 10;
     var imgWidth = 0;
-    var time = new Date();
 
     function loop() {
 
@@ -21,12 +41,7 @@ window.onload = function () {
 
         ctx.drawImage(img, imgWidth - canvas.width, 0);
 
-        ctx.beginPath();
-        ctx.arc(x, y, 10, 0, Math.PI * 2);
-        ctx.fillStyle = "red";
-        ctx.fill();
-        ctx.closePath();
-
+        draw();
 
         imgWidth += scrollspeed;
 
@@ -34,11 +49,6 @@ window.onload = function () {
             imgWidth = 0;
         }
 
-        function jump(event) {
-            if (event.key === "j") {
-                y + 3;
-            }
-        }
         window.requestAnimationFrame(loop);
     }
 
