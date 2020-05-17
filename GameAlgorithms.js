7c029c31d0f5872afd9ecde610fe3ca88a02ea6e
toussmajidi <toussmajidi@yahoo.com>
2020-05-16 22:09:30 -0700

    fixed ball position

--- GameAlgorithms.js
+++ GameAlgorithms.js
@@ -1,20 +1,17 @@
 var background = document.getElementById("background");
-canvas.width = 1000;
-canvas.height = 400;
-var x = canvas.width * (2 / 3);
-var y = canvas.height - 100;
-var by = canvas.height - 100;
+var ball = document.getElementById("ball");
+var bgH = background.style.height;
+var bgW = background.style.width;
+var y = ball.style.top;
 var dy = 0;
 var gravity = 0.5;
 var boxes = [];
 var n = 0;
-var rectX = 0;
-var rectW = 100;
 
 window.addEventListener("keypress", jump);
 
 function jump(event) {
-    if (event.keyCode === 32 && y == by) {
+    if (event.keyCode === 32 && y == bgH) {
         dy = -10;
     }
 
@@ -30,16 +27,6 @@ var im2 = new Image();
     var img2Width = 0;
 
     function scroll() {
-
-        if (y <= (canvas.height - 100)) {
-        dy = dy + gravity;
-    }
-    if (y > (canvas.height - 100)) {
-        y = canvas.height - 100;
-        dy = 0;
-    }
-    y = y + dy;
-
        current -= 1;
         if (direction == "h") {
     background.style.backgroundPosition = current + "px 0";
@@ -49,44 +36,19 @@ var im2 = new Image();
 }
     setInterval("scroll()" , scrollspeed);
 
-
-
-/*function drawCircle() {
-    ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
-    ctx.fillStyle = "blue";
-    ctx.fill();
-    ctx.closePath();
-}
-
-function drawRect() {
-    rectX = n * 3;
-    ctx.beginPath();
-    ctx.rect(rectX, by - 40, rectW, 20);
-    ctx.fillStyle = "green";
-    ctx.fill();
-    ctx.closePath();
-}
-
-
-function randomWidth() {
-
-    rectW = Math.floor(Math.random() * 101);
-
-
-}
-
-function secondlevel() {
-
-    ctx.drawImage(img2, img2Width, 0);
-
-    ctx.drawImage(img2, img2Width - canvas.width, 0);
-
-
-    img2Width += scrollspeed;
-
-    if (img2Width == canvas.width) {
-        img2Width = 0;
+setInterval("ballMove()", 5);
+function ballMove(){
+     if (y <= 290) {
+        dy = dy + gravity;
     }
+    if (y > 290) {
+        y = 290
+        dy = 0;
+    }
+    y = y + dy;
 
-}*/
+    ball.style.top(y);
+
+
+
+}
